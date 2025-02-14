42711bb6efff2f21754ca10a5c36bf0a25c9794f
FranksVilca <franksjaviervilcaquispe@gmail.com>
2025-02-14 11:27:29 -0500

    subido relaciones

--- database/migrations/2025_02_14_153740_create_cita.php
+++ database/migrations/2025_02_14_153740_create_cita.php
@@ -13,16 +13,22 @@ return new class extends Migration
     {
         Schema::create('cita', function (Blueprint $table) {
             $table->increments('cita_id')->unique(); 
-            $table->string('paciente_id',100); 
-            $table->string('tipoCita_id',100); 
-            $table->string('canal_id',100); 
-            $table->string('etiqueta_id',100); 
+            $table->foreignId('paciente_id'); 
+            $table->foreignId('tipoCita_id'); 
+            $table->foreignId('canal_id'); 
+            $table->foreignId('etiqueta_id'); 
             $table->string('motivo_Consulta', 1000); 
             $table->string('estado_Cita', 100); 
             $table->string('colores',100);
             $table->integer('duracion'); 
             $table->date('fecha_cita');
             $table->time('hora_cita');
+
+            $table->foreign('paciente_id')->references('paciente_id')->on('paciente');
+            $table->foreign('tipoCita_id')->references('tipoCita_id')->on('tipoCita');
+            $table->foreign('canal_id')->references('canal_id')->on('canal');
+            $table->foreign('etiqueta_id')->references('etiqueta_id')->on('etiqueta');
+
         });
     }
 
