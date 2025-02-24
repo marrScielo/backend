95ac406e93abc28875d8723f23feceba077dc20b
German <briph21@gmail.com>
2025-02-24 14:32:00 -0500

    integrando el cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'], // Si usas otro puerto se cambia.
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
